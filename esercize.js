80628a03eda399c693f4c25c5cf1463f8bfb62af
Astrid <recoltyy@gmail.com>
2024-03-28 21:03:20 +0100

    idAstridObject

--- esercize.js
+++ esercize.js
@@ -1,1 +1,16 @@
-console.log("hello World")
+const idAstrid = [
+  {
+    firstName: "Astrid",
+    surname: "Zelaya",
+    age: 26,
+    city: "Bergamo",
+    hobby: "Learning new languages",
+    favoriteFood: "Lasagna",
+    favoriteVideoGame: "Genshin Impact",
+    favoriteFilm: "About Time",
+    favoriteBook: "Pet Sematary",
+    petName: "Yuki Tsunoda",
+  },
+];
+
+
