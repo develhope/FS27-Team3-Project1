6427628abc45c52214169f5ddf877783b142ac54
auroraloreti <loretiaurora16@gmail.com>
2024-03-28 20:07:48 +0100

    second commit

--- esercize.js
+++ esercize.js
@@ -1,1 +1,12 @@
-console.log("hello World")
+const idAurora = {
+    firstName: "aurora",
+    surname: "loreti",
+    age: 24,
+    city: "roma",
+    hobby: "music",
+    favoriteFood: "rice with chicken",
+    favoriteVideoGame: "pokemon",
+    favoriteFilm: "it",
+    favoriteBook: "hunger games",
+    petName: "luna"
+}
