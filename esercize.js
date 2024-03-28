1b9c6291d92807b9fa4517d05081d18a2506c8d7
Manuel Cau <caumanuel10@gmail.com>
2024-03-28 21:16:00 +0100

    idManuelObject

--- esercize.js
+++ esercize.js
@@ -1,1 +1,14 @@
-console.log("hello World")
+const idManuel = [
+  {
+    firstName: "Manuel",
+    surname: "Cau",
+    age: 26,
+    city: "Gonnesa",
+    hobby: "Painting",
+    favoriteFood: "Carbonara",
+    favoriteVideoGame: "Rachet&Clank",
+    favoriteFilm: "Eternal Sunshine of a Spotless Mind",
+    favoriteBook: "La Nausea",
+    petName: "Casper",
+  },
+];
