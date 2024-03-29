4e435f01fdc74f2b050c67df503d5d742d0c0e94
Vittoria <vittoriatchotche@gmail.com>
2024-03-29 11:29:51 +0100

    IdVittoriaObject

--- esercize.js
+++ esercize.js
@@ -1,1 +1,14 @@
-console.log("hello World")
+const idVittoria = [
+  {
+    firstName: "Vittoria",
+    surname: "Tchotche",
+    age: "26",
+    city: "Cremona",
+    hobby: "Play piano",
+    favoriteFood: "Panino",
+    favoriteVideoGame: "Scacchi",
+    favoriteFilm: "ApocalypseNow",
+    favoriteBook: "Bible",
+    petName: "",
+  },
+];
